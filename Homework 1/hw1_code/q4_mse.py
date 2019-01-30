b66586d3e6b4e2d0ebec2cc4d0c0c732050a100f
starkof <saystephan@gmail.com>
2019-01-30 11:13:26 +0000

    Implemented function for calculating MSE

--- Homework 1/hw1_code/q4_mse.py
+++ Homework 1/hw1_code/q4_mse.py
@@ -1,17 +1,20 @@
 import numpy as np
 
-def q4_mse(pred_Y, correct_Y):
-# This function calculates the Mean Squared Error given two sets of output
-# values, one set corresponding to the correct values, the other set
-# representing the output values predicted by a regression model
-# INPUT:
-#  pred_Y: a numpy.ndarray vector of type 'float' containing m predicted values
-#  correct_Y: a numpy.ndarray vector of type 'float' containing m correct values
-#
-# OUTPUT:
-#  err: 'float' representing the Mean Squared Error
-#
 
-      # insert your code here
-      
-      return err
+def q4_mse(pred_Y, correct_Y):
+    # This function calculates the Mean Squared Error given two sets of output
+    # values, one set corresponding to the correct values, the other set
+    # representing the output values predicted by a regression model
+    # INPUT:
+    #  pred_Y: a numpy.ndarray vector of type 'float' containing m predicted values
+    #  correct_Y: a numpy.ndarray vector of type 'float' containing m correct values
+    #
+    # OUTPUT:
+    #  err: 'float' representing the Mean Squared Error
+    #
+
+    # insert your code here
+    M = pred_Y.size
+    err = (1/M)*np.sum((pred_Y - correct_Y))**2
+
+    return err
