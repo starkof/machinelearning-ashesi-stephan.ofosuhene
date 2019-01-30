dc24d570d31963bd2abc60fbb2a385dacc4fe2a5
starkof <saystephan@gmail.com>
2019-01-30 11:55:00 +0000

    Completed training algorithm

--- Homework 1/hw1_code/q4_train.py
+++ Homework 1/hw1_code/q4_train.py
@@ -1,26 +1,36 @@
 import numpy as np
 from q4_features import q4_features
 
+
 def q4_train(X, Y, lambdaval, mode):
+    # Trains the regularized least squares regression model using the closed form
+    # solution given the training data X, Y.
+    #
+    # INPUT:
+    #  X: a numpy.ndarray matrix of size [m x d] and type 'float' where each row
+    #     is a d-dimensional input example
+    #  Y: a numpy.ndarray vector of size [m x 1] and type 'float', where the
+    #     i-th element is the correct output value for the i-th input example.
+    #  lambda: 'float' regularization hyperparameter
+    #  mode: specifies the type of features;
+    #        it is a 'str' that can be either 'linear' or 'quadratic'.
+    #
+    # OUTPUT:
+    #  theta: a numpy.ndarray vector of size [n x 1] and type 'float'
+    #         containing the learned model parameters.
+    #
 
-# Trains the regularized least squares regression model using the closed form 
-# solution given the training data X, Y.
-#
-# INPUT:
-#  X: a numpy.ndarray matrix of size [m x d] and type 'float' where each row 
-#     is a d-dimensional input example
-#  Y: a numpy.ndarray vector of size [m x 1] and type 'float', where the 
-#     i-th element is the correct output value for the i-th input example. 
-#  lambda: 'float' regularization hyperparameter
-#  mode: specifies the type of features; 
-#        it is a 'str' that can be either 'linear' or 'quadratic'.
-#
-# OUTPUT:
-#  theta: a numpy.ndarray vector of size [n x 1] and type 'float'
-#         containing the learned model parameters.
-#
+    # print('X =', X)
+    # print('B =', q4_features(X, mode))
+    # print('Y =', Y)
+    # print('lambda =', lambdaval)
+    # print('mode =', mode)
 
-    # insert your code here
+    B = np.matrix(q4_features(X, mode))
 
+    left = B.transpose() * B + (lambdaval[0][0] * np.matrix(np.identity(B.shape[1])))
+    right = B.transpose() * Y
+
+    theta = np.linalg.solve(left, right)
 
     return theta
